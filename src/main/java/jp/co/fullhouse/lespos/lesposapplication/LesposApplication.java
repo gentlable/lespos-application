7e51cc1b458f37faff32e4b6b1e8621f71de99d8
gentlable <shingo.gt.1115@gmail.com>
2023-08-05 12:30:39 +0900

    init

---
+++ src/main/java/jp/co/fullhouse/lespos/lesposapplication/LesposApplication.java
@@ -0,0 +1,13 @@
+package jp.co.fullhouse.lespos.lesposapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/fullhouse/lespos/lesposapplication/LesposApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.fullhouse.lespos.lesposapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
